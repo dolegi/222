1b9995f1f0744611783a82e03d6140cc6975fd1e
x8d <x8d@pm.me>
2020-06-06 17:47:21 +0200

    Minify to 222 bytes

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-javascript:(() => {
-  document.querySelectorAll('*').forEach(el => {
-    let styles = 'background-color: #222; background-image: none;';
-    if (el.tagName === 'A' || el.tagName === 'BUTTON') {
-      styles += 'color: #3366cc; text-decoration: underline;';
-    } else {
-      styles += 'color: #fcfcfc;';
-    }
-    if (el.getAttribute('style')) {
-      el.setAttribute('style', styles + el.getAttribute('style'));
-    } else {
-      el.setAttribute('style', styles);
-    }
-  });
-})()
+javascript:document.querySelectorAll('*').forEach(e=>e.setAttribute('style','background-color:#222;background-image:none;color:#'+(/^A|BU/.test(e.tagName)?'36c;text-decoration:underline;':'eee;')+e.getAttribute('style')))
